ad1dedaa5111887dac80c30f217e6b216bb22427
bharat-02 <malibharatkumar0906@gmail.com>
2026-08-08 04:48:13 +0530

    Hyperparameter training is done

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -27,25 +28,35 @@ def save_object(file_path:str,obj):
 
      
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
-        try:
-            report = {}
-            for i in range(len(models)):
-                model = list(models.values())[i]
-                # Train model
-                model.fit(X_train, y_train)
+def evaluate_model(X_train, y_train,X_test,y_test,models,params):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
     
-                # Predict Testing data
-                y_test_pred = model.predict(X_test)
-    
-                # Get accuracy for the model
-                test_model_score = r2_score(y_test, y_test_pred)
-    
-                report[list(models.keys())[i]] = test_model_score
-    
-                return report
-    
-        except Exception as e:
+    except Exception as e:
             raise CustomException(e, sys)
     
 
